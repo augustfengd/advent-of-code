5175ea5fb3d64895a02f6db0081e50346191a57c
August Feng <augustfengd@gmail.com>
2024-12-02 00:07:13 -0500

    wip: day 1

---
+++ 2024/day.01/Program.fs
@@ -0,0 +1,18 @@
+﻿module String =
+    let split (c : char) (s :string) = s.Split(c)
+
+type Pair =
+    { Left : int
+      Right : int }
+
+let getInput =
+    System.IO.File.ReadAllText("input.txt")
+
+let parseToPair (s :string) =
+    let lines = String.split '\n' s
+    let pair = Seq.map (fun s -> String.split ' ' s) lines
+    42
+
+[<EntryPoint>]
+let main _ =
+    0
