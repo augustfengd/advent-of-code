01c1af697659a8dc63e7eba3c0120cebd41eeb2d
August Feng <augustfengd@gmail.com>
2024-12-26 00:27:48 -0500

    finish day 2

--- 2024/day.02/Program.fs
+++ 2024/day.02/Program.fs
@@ -1,70 +1,59 @@
-﻿open System
+﻿module String =
+    let split (c : char) (s : string) = s.Split(c) 
 
-type State<'s, 'v> = State of ('s -> 'v * 's)
+type State<'s, 'v> = State of ('s -> 's * 'v)
 
 module State =
     let run (State f) state = f state
     
-    let return_ v =
-        let run state = v, state
-        State run
-    
-    let bind f m =
-        let run' state =
-            let v, state' = run m state
-            run (f v) state'
-        State run'
-    
-    let get =
-        let run' state = state, state
-        State run'
-    
-    let put state =
-        let run _ =
-            (), state
-        State run
+    let return_ v = State (fun s -> s, v)
 
-    let zero =
-        let run state =
-            (), state
-        State run
-        
+    let bind m f = State (fun s -> let s', v = run m s in run (f v) s')
+    
+    let get = State (fun s -> s, s)
+    
+    let put s = State (fun _ -> s,())
 
+    let zero = State (fun s -> s, ())
+
+    let apply mf mx=
+        State (fun s ->
+            let s', f = run mf s
+            let s'', v = run mx s'
+            s'', f v)
 type StateBuilder () =
+    member _.Bind(m,f) = State.bind m f
     member _.Return(v) = State.return_ v
     member _.ReturnFrom(m) = m
-    member _.Bind(m,f) = State.bind f m
-    member _.Zero() = State.zero
-    member _.For(sequence: seq<'T>, body: 'T -> State<'s, unit>) : State<'s, unit> =
-        sequence
-        |> Seq.fold (fun acc elem ->
-            State.bind (fun v -> State.bind v) acc
-            ) (State.return_ ())
-
+    
 let state = StateBuilder()
 
-module String =
-    let split (c : char) (s : string) = s.Split(c) 
+module List =
+    let traverseStateM f t =
+        let (>>=) = State.bind
+        let return_ = State.return_
+        
+        let init = return_ []
+        let folder head tail =
+            f head >>= (fun x ->
+            tail   >>= (fun xs -> return_ (x :: xs)))
+        
+        List.foldBack folder t init
 
 module Lib =
-    
     type Direction =
         | Increasing
         | Decreasing
 
-    type Safety =
+    type Safeness =
         | Safe
         | Unsafe
 
-    type Level = int32 list
-        (*
-        { One : int32
-          Two : int32
-          Three : int32
-          Four : int32
-          Five : int32
-          Six : int32 }
-          *)
+    type Progress = {
+        Safeness: Safeness
+        Direction: Direction option
+        Level: int option
+    }
 
     let IsIncreasing a b =
         match a - b with
@@ -72,61 +61,62 @@ module Lib =
         | diff when diff < 0 -> Some Increasing
         | _ -> None
     
-    let validDifference n = n >= 1 && n <= 3 
-    
-    let validate (direction, difference) = state {
-        let! state = State.get
-        match state with
-        | Some (v, Safe) ->
-            if (direction = v || direction = None) && validDifference difference then
-                do! Some (v, Safe) |> State.put
-            else
-                do! Some (v, Unsafe) |> State.put
-        | Some (_, Unsafe)
-        | None -> ()
-    }
-    
-    let computeDirectionAndDifference a b = (IsIncreasing a b, System.Math.Abs(a - b))
+    let computeDirectionAndDistance a (b : int option) =
+        match a, b with
+        | _, None -> None, None
+        | a, Some b -> IsIncreasing a b, Some (System.Math.Abs(a - b))
 
-    // XXX: not the best usage of the state monad? i'm not using the first value of the computed tuple.
-    let check level = state {
-        let pairs = List.pairwise levels
-        for (a,b) in pairs do
-            do! validate (computeDirectionAndDifference a b)
+    let checkDistanceSafeness distance =
+        match distance with
+        | Some n -> if n >= 1 && n <= 3 then Safe else Unsafe
+        | None -> Safe
+    
+    let checkDirectionSafeness  = function
+        | Some Increasing, Some Increasing -> Safe
+        | Some Decreasing, Some Decreasing -> Safe
+        | None, _ -> Safe
+        | _ -> Unsafe
+
+    let check n = state {
+        let! progress = State.get
+        let direction, distance = computeDirectionAndDistance n progress.Level
+
+        let directionSafeness = checkDirectionSafeness (progress.Direction, direction)
+        let distanceSafeness = checkDistanceSafeness distance
+        let safeness = if directionSafeness = Safe && distanceSafeness = Safe then Safe else Unsafe
+
+        do! State.put { progress with Safeness = safeness ; Direction = direction ; Level = Some n }
+        return safeness
     }
+
+    let foldSafeness = List.fold (function current -> function | Unsafe -> Unsafe | _ -> current) Safe
+
+    let checkReport levels =
+       let init = { Safeness = Safe ; Direction = None ; Level = None }
+       let _, operations = State.run (List.traverseStateM check levels) init
+       foldSafeness operations
 
     let getInput ()  = System.IO.File.ReadAllText "input.txt"
-    
+
     let toLines = String.split '\n'
 
     let parse =
         toLines
         >> Array.map (String.split ' ')
         >> Array.map (Array.map System.Int32.Parse)
-        >> Array.map (fun v -> { One = v.[0]
-                                 Two = v.[1]
-                                 Three = v.[2]
-                                 Four = v.[3]
-                                 Five = v.[4]
-                                 Six = v.[5] })
 
-    let runCheck level =
-        State.run (check level) None
+    let run  =
+        getInput
+        >> parse
+        >> Array.map (Array.toList >> checkReport)
+        >> Array.sumBy (function | Safe -> 1 | Unsafe -> 0)
 
 module App =
     open Lib
-
     let part1 () =
-        let s = getInput ()
-        let levels = parse s
-        let runs = Array.map runCheck levels
-        runs
-        |> Array.map snd
-        |> Array.choose id
-        |> Array.map snd
-        |> Array.sumBy (function | Safe -> 1 | Unsafe -> 0)
+        Lib.run ()
 
 [<EntryPoint>]
 let main _ =
-    App.part1 () |> printfn "%d" 
+    App.part1 () |> printfn "%A"
     0
